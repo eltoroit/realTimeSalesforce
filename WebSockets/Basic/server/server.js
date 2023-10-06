ea33bcd1f983bf2d77c2603b227dcb2371ab5cf3
Your Name <you@example.com>
2023-10-06 18:49:46 -0400

    Basic Socket.IO demo

--- WebSockets/Basic/server/server.js
+++ WebSockets/Basic/server/server.js
@@ -1,27 +1,38 @@
+import http from "http";
 import express from "express";
-import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+export default class WebSockets {
+	io;
+	app;
 
-app.get("/", (req, res) => {
-	let url = new URL("./index.html", import.meta.url).pathname;
-	res.sendFile(url.replace("%20", " "));
-});
+	async initializeServer() {
+		this.app = express();
+		this.app.set("view engine", "ejs");
+		this.app.use(express.static("public"));
+		const httpServer = http.createServer(this.app);
+		this.io = new Server(httpServer);
 
-io.on("connection", (socket) => {
-	console.log(`${socket.id} connected`);
-	socket.on("disconnect", () => {
-		console.log(`${socket.id} disconnected`);
-	});
-	socket.on("chat message", (msg) => {
-		console.log("message: " + msg);
-		io.emit("chat message", msg);
-	});
-});
+		this.io.on("connection", (socket) => {
+			console.log(`${socket.id} connected`);
+			socket.on("disconnect", () => {
+				console.log(`${socket.id} disconnected`);
+			});
+			socket.on("chat message", (msg) => {
+				console.log("message: " + msg);
+				this.io.emit("chat message", msg);
+			});
+		});
 
-server.listen(3000, () => {
-	console.log("server running at http://localhost:3000");
-});
+		this.app.get("/", (req, res) => {
+			res.render("pages/index");
+		});
+
+		httpServer.listen(3000, () => {
+			console.log("server running at http://localhost:3000");
+		});
+	}
+}
+
+let ws = new WebSockets();
+ws.initializeServer();
