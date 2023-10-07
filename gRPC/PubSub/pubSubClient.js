4cdacbf97f45036e4882297d1ca5df5abd9502f9
Your Name <you@example.com>
2023-10-07 10:29:48 -0400

    Fixed PubSub weird timing issue

--- gRPC/PubSub/pubSubClient.js
+++ gRPC/PubSub/pubSubClient.js
@@ -2,8 +2,8 @@ import PubSubApiClient from "salesforce-pubsub-api-client";
 
 async function run() {
 	let client;
-	let events;
-	let lastReplayId;
+	// let events = {};
+	// let lastReplayId = 0;
 
 	const initializeClient = async () => {
 		client = new PubSubApiClient();
@@ -11,34 +11,55 @@ async function run() {
 	};
 
 	const subscribe = async (channel) => {
-		return new Promise(async (resolve, reject) => {
-			// let promise = client.subscribe(channel, 1);
-			let promise = client.subscribeFromReplayId(channel, 2, lastReplayId);
-			// This wait seems to solve a synchronization problem
-			await new Promise((resolve2) => setTimeout(resolve2, 1e2));
-			promise
+		console.log(`${new Date().toJSON()} Subscribing`);
+		let subscriberPromise = new Promise((resolve2, reject2) => {
+			// let promise = client.subscribeFromReplayId(channel, 2, lastReplayId);
+			let listenerPromise = client.subscribe(channel, 1);
+			listenerPromise
 				.then((listener) => {
 					listener.on("data", (event) => {
-						lastReplayId = Math.max(lastReplayId, event.replayId);
-						events[event.replayId] = event;
+						// lastReplayId = Math.max(lastReplayId, event.replayId);
+						// events[event.replayId] = event;
 					});
 					listener.on("lastevent", (event) => {
-						console.log(`Received ${Object.keys(events).length} event`);
+						// console.log(`Received ${Object.keys(events).length} event`);
 						console.log(`Reached last requested event on channel ${listener.getTopicName()}.`);
-						resolve();
+						resolve2();
+						abortHack();
 					});
 				})
 				.catch((error) => {
 					console.error(error);
 					console.log("ABORTED");
-					reject(error);
+					reject2(error);
 				});
 		});
+
+		// This wait seems to solve a synchronization problem
+		let forceTimer;
+		let promises = [];
+		let forceTimerResolve;
+		const abortHack = () => {
+			if (forceTimerResolve) {
+				clearTimeout(forceTimer);
+				forceTimerResolve();
+				console.log(`${new Date().toJSON()} Hack aborted`);
+			}
+		};
+		let timerPromise = new Promise((resolve2, reject2) => {
+			forceTimerResolve = resolve2;
+			forceTimer = setTimeout(() => {
+				forceTimerResolve = null;
+				console.log(`${new Date().toJSON()} Hack ended`);
+				resolve2();
+			}, 5e3);
+		});
+		promises.push(subscriberPromise);
+		promises.push(timerPromise);
+		await Promise.allSettled(promises);
 	};
 
 	try {
-		events = {};
-		lastReplayId = 0;
 		let channel = "/event/CloudNews__e";
 		// let channel = "/data/AccountChangeEvent";
 
